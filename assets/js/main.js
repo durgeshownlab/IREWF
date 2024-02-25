22f28b09ef5c136e5e6a64f35fbe4611461db10a
Durgesh Singh <105972298+durgeshownlab@users.noreply.github.com>
2024-02-26 00:51:58 +0530

    contact us made 50 %
    contact us made 50 %

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,25 @@ $(document).ready(function(){
         console.log('menu bar click ')
         $('.c-menu-bar-container').toggle();
     });
+
+    // code for navigating throw the info tab 
+    $(document).on('click', '.info-tab-item', function(e){
+        console.log($(this).data('id'))
+
+        let target=$($(this).data('id'));
+
+        // $(`${id}`).css('padding-top', '150px')
+        
+        $('.info-tab-item').removeClass('active');
+        $(this).addClass('active')
+
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top - 150
+            }, 500);
+        }
+
+    });
 });
